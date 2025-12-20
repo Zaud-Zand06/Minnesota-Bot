0643613c6db9e3da16a80bbef5936493f359de68
Alex <Alex3388@live.ca>
2025-12-19 17:51:21 -0800

    add pingler command

---
+++ commands/utility/thePingler.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require("discord.js");
+require("dotenv").config();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pingler")
+    .setDescription("for getting someones attention")
+    .addUserOption((option) =>
+      option.setName("who").setDescription("who do you need").setRequired(true),
+    )
+    .addUserOption((option) =>
+      option
+        .setName("count")
+        .setDescription("how many times to ping")
+        .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const target = interaction.options.getMember("who").id;
+    const pingAmount = interaction.options.getInterget("count");
+    if (pingAmount > 25) {
+      return interaction.reply({
+        content: "too many pings! max is 25",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({ content: "pinging....", ephemeral: true });
+      for (let i = 0; i < pingAmount; i++) {
+        await interaction.channel.send({ content: `<@${target}>` });
+      }
+    }
+  },
+};
